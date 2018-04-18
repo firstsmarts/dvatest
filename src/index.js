8b2c8bcaedfe501b1b6ec5c850ba01f4afaf2c95
chengkun <chengkun@antiy.cn>
2018-04-18 18:19:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import dva from 'dva'
-import createHistory from 'history/createHashHistory';
+import createHistory from 'history/createBrowserHistory';
 
 import './index.css'
 
--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {Router, Route, Switch} from 'dva/router'
 import dynamic from 'dva/dynamic'
 import user from './models/user'
-import global from './models/global'
 const Index = (app) => dynamic({
     app,
     models: () => [global],
@@ -11,7 +10,7 @@ const Index = (app) => dynamic({
 
 const User = (app) => dynamic({
     app,
-    models: () => [user],
+    models: () => [import('./models/global')],
     component: () => import('./pages/user')
 })
 
